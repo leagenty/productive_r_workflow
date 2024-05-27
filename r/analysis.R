3e680c2e27e53296b218391dd4b91836146199fe
Léa Genty <genty.lea@hotmail.fr>
2024-05-27 15:56:27 +0200

    Initial commit

---
+++ r/analysis.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+
+# Ouvrir le doc depuis un projet
+
+data <- read.csv("input/data.csv") #comme nous sommes dans un projet R je n'ai plus besoin 
+
+summary(data)
+
+# Correct script
+
+print(round(mean(subset(na.omit(data), species == "Adelie" & island == "Torgersen")$bill_length_mm),2))
+print(round(mean(subset(na.omit(data), species == "Adelie" & island == "Biscoe")$bill_length_mm),2))
+print(round(mean(subset(na.omit(data), species == "Adelie" & island == "Dream")$bill_length_mm),2))
+
+
+
+# Lire un fichier excel
+
+data2 <- read_excel("input/data.xlsx") #pour lire direct le fichier excel
+
+# Create functions
+
+print(
+  round(mean(subset(
+    na.omit(data), species == "Adelie" & island == "Torgersen"
+  )$bill_length_mm), 2)
+)
+
+print(
+  round(mean(subset(
+    na.omit(data), species == "Adelie" & island == "Biscoe"
+  )$bill_length_mm), 2)
+)
+print(
+  round(mean(subset(
+    na.omit(data), species == "Adelie" & island == "Dream"
+  )$bill_length_mm), 2)
+)
+## ici c'est dupliqué 3 fois
